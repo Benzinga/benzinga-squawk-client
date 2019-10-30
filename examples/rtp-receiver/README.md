e564a0d7c3ce84526d3e08c939d63fbad4251eb7
Hemraj <hemrajsinh@benzinga.com>
2019-10-30 22:00:40 +0530

    rtp-receiver WebSocket Client

---
+++ examples/rtp-receiver/README.md
@@ -0,0 +1,37 @@
+# rtp-receiver - Simple WebScoket client to receive Squawk RTP stream
+
+## Overview
+Squawk WebSocket client implementation for connection to squawk and receiving RTP stream. This includes connecting to squawk WebSocket server and exchanging messages, mainly authentication and SDP negotiation, for receiving RTP stream on a specific IP:Port. You can review predefined message types and formats at https://docs.benzinga.io/
+
+## Prerequisites
+1. [Java](https://www.oracle.com/technetwork/java/javase/downloads/index.html) >=1.8
+2. [Maven](https://maven.apache.org/download.cgi)
+3. API key for Benzinga Squawk. Contact Benzinga licensing team at  licensing@benzinga.com, if you don't have one.
+4. Squawk WebSocket address. Contact Benzinga licensing team at  licensing@benzinga.com for that.
+5. Publicly open and accessible IP:Port on your end.
+
+## Installing and Executing
+
+~~~bash
+git clone https://github.com/Benzinga/benzinga-squawk-client.git
+cd benzinga-squawk-client/examples/rtp-receiver
+mvn clean install
+java -jar target/rtp-receiver-0.0.1-jar-with-dependencies.jar
+~~~
+
+Please note that the execution is dependent on configuration, which can be done by either changing config values in `src/main/resources/application.conf` or setting environment variables. The configuration parameter in `application.conf` includes the following:
+
+| Config parameter       |  Equivalent Env variable  | Description
+| ------------- |-------------| -----
+| bz.squawk.apiKey      | BZ_SQUAWK_APIKEY | API key received from Benzinga Licencing team 
+| bz.squawk.room      | BZ_SQUAWK_ROOM | Room to join. It must be `PRO`
+| bz.squawk.role      | BZ_SQUAWK_ROLE | Room to join. It must be `rtpreceiver`
+| receiver.ip      | RECEIVER_IP | The IP address where you want to receive RTP stream. It must be publicly accessible IP
+| receiver.port      | RECEIVER_PORT | An open port to receive RTP stream.
+
+ 
+Please note that if you set the environment variable for a config parameter, then the `application.conf` value will be overridden by the env value. 
+
+## Verifying 
+
+Once the authentication and SDP negotiation is successful, you should start receiving stream at configured `IP:Port`. The generated SDP offer will be saved under `<USER_HOME>/Documents/bz-squawk-sdpoffer/inputAudio.sdp` You can use this file to verify the streaming. Open this file in VLC player (Media > Open File) and wait for the next squawk from the Benzinga team.
---
+++ examples/rtp-receiver/src/main/java/com/benzinga/squawk/SquawkWSClient.java
@@ -0,0 +1,195 @@
+package com.benzinga.squawk;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.uuid.Generators;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * Class {@code SquawkWSClient} is the Web Socket client,
+ * which connects to squawk WS and do message exchanges for
+ * authentication, SDP negotiation, logout etc
+ *
+ */
+public class SquawkWSClient extends WebSocketClient {
+   
+  private static final Logger log = LoggerFactory.getLogger(SquawkWSClient.class);
+  private final Gson gson = new GsonBuilder().create();
+  private static Config conf;  
+  
+  private static final String MESSAGE_TYPE_AUTH = "auth";
+  private static final String MESSAGE_TYPE_PING = "ping";
+  private static final String MESSAGE_TYPE_SDP_OFFER = "sdp-offer";
+  private static final String MESSAGE_TYPE_LOGOUT = "logout";
+  private static final String MESSAGE_TYPE_MEDIA_OVERRIDE = "media-override";
+  
+  public SquawkWSClient(String serverURI) throws URISyntaxException {
+    super(new URI(serverURI));        
+    this.setConnectionLostTimeout(30);
+  }
+  
+  public SquawkWSClient( URI serverUri , Draft draft ) {
+      super( serverUri, draft );
+  }
+
+  public SquawkWSClient( URI serverURI ) {
+      super( serverURI );
+  }
+
+  public SquawkWSClient( URI serverUri, Map<String, String> httpHeaders ) {
+      super(serverUri, httpHeaders);
+  }
+
+  @Override
+  public void onOpen( ServerHandshake handshakedata ) {
+      log.info("WebSocket connection opened");
+      sendAuthMessage();
+  }
+
+  @Override
+  public void onMessage( String message ) {
+      log.info("Message Received {}", message);
+      JsonObject msg = gson.fromJson(message, JsonObject.class);
+        switch (msg.get("type").getAsString()) {
+          case MESSAGE_TYPE_AUTH:
+            if (msg.has("error")) {
+              log.error("Authentication failed {}", msg.get("error").getAsString());
+            } else {
+              this.sendSdpOffer(sdpOffer());
+              log.error("Authentication successful. Sending SDP Offer");
+            }
+            break;
+          case MESSAGE_TYPE_SDP_OFFER:
+            if (msg.has("error")) {
+              log.error("Failed to negotiate SDP. Error: {}", msg.get("error").getAsString());              
+            } else {
+              log.info("SDP negitiation successfull");
+            }
+            break;
+          case MESSAGE_TYPE_MEDIA_OVERRIDE:  
+            log.info("Received media-override message. Session ended. Looks like signed in from another session using same API key.");
+            log.info("Closing WebSocket connection.");
+            this.close();
+            break;           
+          case MESSAGE_TYPE_PING:  
+            log.info("Pong Received");
+            break;         
+        }     
+  }
+
+  @Override
+  public void onClose( int code, String reason, boolean remote ) {
+      log.info( "Connection closed by " + ( remote ? "remote peer" : "us" ) + " Code: " + code + " Reason: " + reason );
+  }
+
+  @Override
+  public void onError( Exception ex ) {
+    log.error("Error occured: ", ex);      
+  }
+  
+  @Override
+  public void sendPing() {
+    log.info("Sending ping");
+    JsonObject pingObj = new JsonObject();
+    pingObj.addProperty("id", Generators.timeBasedGenerator().generate().toString());
+    pingObj.addProperty("type", MESSAGE_TYPE_PING);
+    this.send(pingObj.toString());
+  }
+
+  private void sendAuthMessage() {
+    log.info("Authenticating");
+    JsonObject authObj = new JsonObject();
+    authObj.addProperty("id", Generators.timeBasedGenerator().generate().toString());
+    authObj.addProperty("role", conf.getString("bz.squawk.role"));
+    authObj.addProperty("type", MESSAGE_TYPE_AUTH);
+    authObj.addProperty("apikey", conf.getString("bz.squawk.apiKey"));
+    authObj.addProperty("room", conf.getString("bz.squawk.room"));
+    this.send(authObj.toString());    
+  }
+  
+  private void sendSdpOffer(String sdpOffer) {
+    log.info("Sending SDP Offer");
+    JsonObject receiveMediaMessage = new JsonObject();
+    receiveMediaMessage.addProperty("id", Generators.timeBasedGenerator().generate().toString());
+    receiveMediaMessage.addProperty("type", MESSAGE_TYPE_SDP_OFFER);
+    receiveMediaMessage.addProperty("sdpOffer", sdpOffer);
+    this.send(receiveMediaMessage.toString());
+  }
+  
+  private void sendLogoutMessage() {
+    log.info("Sending Logout message");
+    JsonObject logoutMessage = new JsonObject();
+    logoutMessage.addProperty("id", Generators.timeBasedGenerator().generate().toString());
+    logoutMessage.addProperty("type", MESSAGE_TYPE_LOGOUT);    
+    this.send(logoutMessage.toString());
+  }  
+  
+  private String sdpOffer() {
+    String rtpSdpOffer = "v=0\n" + 
+        "t=0 0\n" + 
+        "m=audio " + conf.getString("receiver.port") + " RTP/AVP 98\n" + 
+        "c=IN IP4 " + conf.getString("receiver.ip") + "\n" + 
+        "a=recvonly\n" + 
+        "a=rtpmap:98 opus/48000/2\n" + 
+        "a=fmtp:98 stereo=0; sprop-stereo=0; useinbandfec=1";
+    log.info("SDP Offer \n{}", rtpSdpOffer);
+    this.writeSdpOffertoFile(rtpSdpOffer);
+    return rtpSdpOffer;
+  } 
+  
+  private void writeSdpOffertoFile(String rtpSdpOffer) {
+    String path = System.getProperty("user.home") + File.separator + "Documents";
+    path += File.separator + "bz-squawk-sdpoffer";
+    File customDir = new File(path);
+
+    if (!customDir.exists()) {
+      log.info(customDir + " created");
+      customDir.mkdirs();    
+    } 
+        
+    File sdpOfferfile = new File(customDir.getPath() + File.separator + "inputAudio.sdp");
+    BufferedWriter writer;
+    try {
+      writer = new BufferedWriter(new FileWriter(sdpOfferfile));
+      writer.write(rtpSdpOffer);     
+      writer.close();
+      log.info("SDP Offer input file created at {}", sdpOfferfile.getPath());
+      log.info("You can open this file in VLC to play live squawk audio stream");
+    } catch (IOException e) {
+      log.error("Error occured while writting the SDP offer to file" , e);
+    }    
+    
+  }
+    
+  public static void main( String[] args ) throws URISyntaxException {
+    conf = ConfigFactory.load(); 
+    log.info("Squawk Address : {}", conf.getString("bz.squawk.addr")); 
+    SquawkWSClient c = new SquawkWSClient(conf.getString("bz.squawk.addr")); // more about drafts here: http://github.com/TooTallNate/Java-WebSocket/wiki/Drafts
+    c.connect();
+    Runtime.getRuntime().addShutdownHook(new Thread() 
+    { 
+      public void run() 
+      { 
+        if (c.isOpen()) {
+          log.info("Shutting down WebSocket Client!"); 
+          c.sendLogoutMessage();
+          c.close();
+        }
+      } 
+    }); 
+  }
+}
